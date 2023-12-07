c8c634523d034c6280fc0e95d1bed05c32369781
elvisowusu <owusuelvisgyasi@gmail.com>
2023-12-07 18:44:23 +0000

    Refactor background color in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import MainComponent from "./MainComponent"
 function App() {
 
   return (
-    <div className='font-RubikFont bg-Verydarkblue sm:bg-red-400  md:bg-yellow-400 lg:bg-red-500 xl:bg-Verydarkblue'>
+    <div className='font-RubikFont bg-Verydarkblue'>
       <MainComponent/>
     </div>
   )
