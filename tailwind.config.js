2ef7ca575ac91dd57195008a20b140b1eaa3d440
elvisowusu <owusuelvisgyasi@gmail.com>
2023-12-07 10:54:48 +0000

    Update App component and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+          BlueColor: 'hsl(246, 80%, 60%)',
+          Lightredwork: 'hsl(15, 100%, 70%)',
+          Softblueplay: 'hsl(195, 74%, 62%)',
+          Lightredstudy: 'hsl(348, 100%, 68%)',
+          Limegreenexercise: 'hsl(145, 58%, 55%)',
+          Violetsocial: 'hsl(264, 64%, 52%)',
+          Softorangeselfcare: 'hsl(43, 84%, 65%)',
+          Darkblue: 'hsl(235, 46%, 20%)',
+          Desaturatedblue: 'hsl(235, 45%, 61%)',
+          PaleBlue:'hsl(236, 100%, 87%)',
+          Verydarkblue: 'hsl(226, 43%, 10%)',
+      }
+    },
   },
   plugins: [],
 }
